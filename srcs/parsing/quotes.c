8b8a048353e53a9e595e50f26a5b099e2882ed11
Taj Bhuiyan <tbhuiyan@f1br10s3.clusters.42paris.fr>
2025-11-25 22:39:14 +0100

    prototype de la gestion des quotes : test unitaire a faire.

---
+++ srcs/parsing/quotes.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   quotes.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbhuiyan <tbhuiyan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/25 22:22:02 by tbhuiyan          #+#    #+#             */
+/*   Updated: 2025/11/25 22:31:32 by tbhuiyan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+char	*remove_quotes(char *str, t_type_quote quote_type)
+{
+	char	*res;
+	int		len;
+
+	if (quote_type == NO_QUOTE)
+		return(ft_strdup(str));
+	len = ft_strlen(str);
+	res = ft_substr(str, 1, len - 2);
+	return (res);
+}
+
+void	process_quotes(t_token *token)
+{
+	t_token	*cur;
+	char	*new_str;
+
+	cur = token;
+	while (cur)
+	{
+		if (cur->type == WORD && cur->quote_type != NO_QUOTE)
+		{
+			new_str = remove_quotes(cur->str, cur->quote_type);
+			if (new_str)
+			{
+				free(cur->str);
+				cur->str = new_str;
+			}
+		}
+		cur = cur->next;
+	}
+}
